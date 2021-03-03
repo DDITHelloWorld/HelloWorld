2c4d10469bdb162e5a7ef6ac3f330ea6dcc895f4
DESKTOP-3KJ2DR5\PC-14 <tnal9710@gmail.com>
2021-03-03 19:00:12 +0900

    잘됨!

--- HelloWorld_01/HelloWorld/src/kr/or/ddit/web/WebController.java
+++ HelloWorld_01/HelloWorld/src/kr/or/ddit/web/WebController.java
@@ -1,5 +1,4 @@
 package kr.or.ddit.web;
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -7,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kr.or.ddit.web.*;
 
 public class WebController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -28,7 +29,6 @@ public class WebController extends HttpServlet {
 		// 1. 사용자의 요청 정보 가져오기
 		String uri = req.getRequestURI();  // 전체요청 URI
 		
-		
 		// 원하는 요청 URI
 		uri = uri.substring(req.getContextPath().length());
 		System.out.println("uri=>" + uri);
@@ -37,7 +37,7 @@ public class WebController extends HttpServlet {
 		IAction action = null;
 
 		action = URIActionMapper.getAction(uri);
-
+		System.out.println("action=>" + action);
 		if(action==null) {  // 실행할 uri가 없으면 404에러 처리
 			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		}else {
@@ -47,11 +47,12 @@ public class WebController extends HttpServlet {
 			if(viewPage!=null) {
 				if(action.isRedirect()) {
 					resp.sendRedirect(req.getContextPath() + viewPage);
-				
+					System.out.println("isRedirect=>" + uri);
 				}else {
 					RequestDispatcher rd = 
 							req.getRequestDispatcher("/WEB-INF/view" + viewPage);
 					rd.forward(req, resp);
+					System.out.println("uri=>" + uri);
 				}
 				
 			}
