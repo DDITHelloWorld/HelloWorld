ca8a30a7af44b580f2d47fe442ce3e5e92023bc9
KrGil <kanjjang89@naver.com>
2021-03-04 10:02:08 +0900

    Add files via upload

---
+++ HelloWorld_01/HelloWorld/src/kr/or/ddit/vo/AttractionVO.java
@@ -0,0 +1,84 @@
+package kr.or.ddit.vo;
+
+public class AttractionVO {
+	private int promotion_no;
+	private int prod_views;
+	private int month_destination;
+	private String prod_date;
+	private int prod_state;
+	private int prod_sale;
+	private int prod_price;
+	private int guide_no;
+	private String prod_lgu;
+	private String prod_name;
+	private int prod_no;
+	
+	public int getPromotion_no() {
+		return promotion_no;
+	}
+	public void setPromotion_no(int promotion_no) {
+		this.promotion_no = promotion_no;
+	}
+	public int getProd_views() {
+		return prod_views;
+	}
+	public void setProd_views(int prod_views) {
+		this.prod_views = prod_views;
+	}
+	public int getMonth_destination() {
+		return month_destination;
+	}
+	public void setMonth_destination(int month_destination) {
+		this.month_destination = month_destination;
+	}
+	public String getProd_date() {
+		return prod_date;
+	}
+	public void setProd_date(String prod_date) {
+		this.prod_date = prod_date;
+	}
+	public int getProd_state() {
+		return prod_state;
+	}
+	public void setProd_state(int prod_state) {
+		this.prod_state = prod_state;
+	}
+	public int getProd_sale() {
+		return prod_sale;
+	}
+	public void setProd_sale(int prod_sale) {
+		this.prod_sale = prod_sale;
+	}
+	public int getProd_price() {
+		return prod_price;
+	}
+	public void setProd_price(int prod_price) {
+		this.prod_price = prod_price;
+	}
+	public int getGuide_no() {
+		return guide_no;
+	}
+	public void setGuide_no(int guide_no) {
+		this.guide_no = guide_no;
+	}
+	public String getProd_lgu() {
+		return prod_lgu;
+	}
+	public void setProd_lgu(String prod_lgu) {
+		this.prod_lgu = prod_lgu;
+	}
+	public String getProd_name() {
+		return prod_name;
+	}
+	public void setProd_name(String prod_name) {
+		this.prod_name = prod_name;
+	}
+	public int getProd_no() {
+		return prod_no;
+	}
+	public void setProd_no(int prod_no) {
+		this.prod_no = prod_no;
+	}
+	
+	
+}
