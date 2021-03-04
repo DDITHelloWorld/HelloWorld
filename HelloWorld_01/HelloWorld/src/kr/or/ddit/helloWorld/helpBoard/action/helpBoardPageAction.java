646f40e4b2c8c2c45814e639e47ab2aa2901273d
KrGil <kanjjang89@naver.com>
2021-03-04 10:03:42 +0900

    Add files via upload

---
+++ HelloWorld_01/HelloWorld/src/kr/or/ddit/helloWorld/helpBoard/action/helpBoardPageAction.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.helloWorld.helpBoard.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.ddit.web.IAction;
+
+public class helpBoardPageAction implements IAction {
+
+	@Override
+	public boolean isRedirect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		return "/helpBoardPage/helpBoardView.jsp";
+	}
+
+}
